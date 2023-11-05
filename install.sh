bb52adfcb98b36619ae8ccbd843a61872ed18ce6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-11-04 20:41:09 -0700

    remove unused scripts

--- install.sh
+++
@@ -1,3 +0,0 @@
-sudo cp -r acpi /etc
-sudo chmod +x /etc/acpi/actions/tablet-mode.sh
-sudo systemctl restart acpid
--- uninstall.sh
+++
@@ -1,2 +0,0 @@
-sudo rm /etc/acpi/events/tablet-mode /etc/acpi/actions/tablet-mode.sh /etc/actions/tablet-mode.txt
-sudo systemctl restart acpid
